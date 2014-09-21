0006712837c55ca2966f5203bbb8a49c1ac84b5b
Jeff Kowalski <jeff.kowalski@gmail.com>
2014-09-20 21:59:33 -0700

    Added defvars for some local variables to quiet byte-compiler

--- org-cua-dwim.el
+++ org-cua-dwim.el
@@ -6,74 +6,77 @@
 ;; Maintainer: Matthew L. Fidler
 ;; Created: Thu Dec  8 15:06:13 2011 (-0600)
 ;; Version: 0.5
-;; Last-Updated: Thu Feb  2 23:30:32 2012 (-0600)
-;;           By: Matthew L. Fidler
-;;     Update #: 218
-;; URL: 
+;; Last-Updated: Sat Sep 20 2014
+;;           By: Jeff Kowalski
+;;     Update #: 218.1
+;; URL:
 ;; Keywords: org-mode cua-mode
-;; Compatibility: Emacs 23 
-;; 
+;; Compatibility: Emacs 23
+;;
 ;; Features that might be required by this library:
 ;;
 ;;   None
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
+;;
+;;; Commentary:
+;;
 ;;  To use this, just put in load path and then:
 ;;
 ;;  (require 'org-cua-dwim)
 ;;  (org-cua-dwim-activate)
-;;  To activate, 
+;;  To activate,
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 02-Feb-2012    Matthew L. Fidler  
+;; 20-Sep-2014    Jeff Kowalski
+;;    Last-Updated: Sat Sep 20          2014              (Jeff Kowalski)
+;;    Added defvars for some local variables to quiet byte-compiler
+;; 02-Feb-2012    Matthew L. Fidler
 ;;    Last-Updated: Thu Feb  2 23:27:14 2012 (-0600) #216 (Matthew L. Fidler)
 ;;    Turned off automatic activation.
-;; 11-Dec-2011    Matthew L. Fidler  
+;; 11-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Sun Dec 11 10:01:49 2011 (-0600) #212 (Matthew L. Fidler)
 ;;    Turned on Delete-Selection-mode whe shift-selection-mode is enabled
-;; 11-Dec-2011    Matthew L. Fidler  
+;; 11-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Sun Dec 11 09:58:30 2011 (-0600) #205 (Matthew L. Fidler)
 ;;    Added more CUA advices.
-;; 11-Dec-2011    Matthew L. Fidler  
+;; 11-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Sun Dec 11 00:20:36 2011 (-0600) #185 (Matthew L. Fidler)
 ;;    Another major bug fix.  It seems to work now.
-;; 10-Dec-2011    Matthew L. Fidler  
+;; 10-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Sat Dec 10 23:14:30 2011 (-0600) #133 (Matthew L. Fidler)
 ;;    Another Major bug fix when woring on a Mac.  Hopefully this works.
-;; 09-Dec-2011    Matthew L. Fidler  
+;; 09-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Fri Dec  9 09:13:37 2011 (-0600) #69 (Matthew L. Fidler)
 ;;    Last time the system worked because an error removed the
 ;;    pre-command-hook. This time the system works without turning off
 ;;    the pre-command-hook.
-;; 08-Dec-2011    Matthew L. Fidler  
+;; 08-Dec-2011    Matthew L. Fidler
 ;;    Last-Updated: Thu Dec  8 17:00:24 2011 (-0600) #6 (Matthew L. Fidler)
 ;;    Initial Release
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 ;;;###autoload
 (defun org-cua-dwim-turn-on-org-cua-mode-partial-support ()
@@ -88,7 +91,7 @@ shift-selection-mode is available."
   (set (make-local-variable 'org-cua-dwim-was-move) nil)
   (set (make-local-variable 'shift-select-mode) nil))
 
-;;;###autoload 
+;;;###autoload
 (defun org-cua-dwim-activate ()
   "Activate org-cwim-dwim"
   (add-hook 'org-mode-hook 'org-cua-dwim-turn-on-org-cua-mode-partial-support))
@@ -96,6 +99,9 @@ shift-selection-mode is available."
 (defvar org-cua-dwim-was-move nil)
 (defvar org-cua-dwim-debug nil)
 (defvar org-cua-dwim t)
+(defvar org-cua-dwim-shift-translated)
+(defvar cua--last-region-shifted)
+(defvar cua--explicit-region-start)
 
 (defadvice handle-shift-selection (around org-cua-dwim)
   (let ((is-org-mode (and (not (minibufferp))
